5c05f85d712a5d05f96810c8da6e2d5c8d8560bb
valaxy <valaxy.tian@gmail.com>
2014-12-30 20:34:45 +0800

    update demo

--- demo/basic/index.js
+++ demo/basic/index.js
@@ -1,52 +1,41 @@
 define(function (require, exports) {
 
-	var LinearLayout = require('../../src/linear-layout');
+	var boxLayout = require('../../src/box-layout');
 
 	exports.init = function () {
 
-		var rootLayout = new LinearLayout({
-			parent: null,
+var $dom = boxLayout.init({
+	_schema: 'linear',
+	isHor: false,
+	boxes: [{
+		_schema: 'box',
+		size: 100,
+		domSelector: '.box0'
+	}, {
+		_schema: 'linear',
+		isHor: true,
+		size: 'auto',
+		boxes: [{
 			_schema: 'linear',
 			isHor: false,
-			boxes: [
-				{
-					_schema: 'box',
-					size: 100,
-					domSelector: '.box0'
-				},
-				{
-					_schema: 'linear',
-					isHor: true,
-					size: 'auto',
-					boxes: [
-						{
-							_schema: 'linear',
-							isHor: false,
-							size: 300,
-							boxes: [
-								{
-									_schema: 'box',
-									size: 200,
-									domSelector: '.box1'
-								},
-								{
-									_schema: 'box',
-									size: 'auto',
-									domSelector: '.box2'
-								}
-							]
-						},
-						{
-							_schema: 'box',
-							size: 'auto',
-							domSelector: '.box3'
-						}
-					]
-				}
-			]
-		});
+			size: 300,
+			boxes: [{
+				_schema: 'box',
+				size: 200,
+				domSelector: '.box1'
+			}, {
+				_schema: 'box',
+				size: 'auto',
+				domSelector: '.box2'
+			}]
+		}, {
+			_schema: 'box',
+			size: 'auto',
+			domSelector: '.box3'
+		}]
+	}]
+})
 
-		$('.everything').append(rootLayout.$dom);
-
+$('.everything').append($dom)
 	}
-});
+})
