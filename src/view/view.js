e049cd959c1f3700ea7c3d56e8852ddfc56d17b2
valaxy <valaxy.tian@gmail.com>
2015-01-30 14:40:50 +0800

    fix bug

--- src/view/view.js
+++ src/view/view.js
@@ -2,7 +2,7 @@ define(function (require) {
 	var $ = require('jquery')
 
 	var View = function (options) {
-		this._$dom = $(options.selector)
+		this._$dom = $(options.selector).detach()
 
 		// make css API
 		this._$dom.addClass('view')
